9371c3c02ae4f23a340c632003237d42985b70f2
qPels01 <petrlensin011@gmail.com>
2025-05-30 12:04:40 +0300

    first commit

---
+++ frontend/src/views/RegisterPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <label>Email</label>
+    <input type="text" required>
+    <label>Имя пользователя</label>
+    <input type="text" required>
+    <label>Пароль</label>
+    <input type="password" required>
+
+    <button>Подтвердить</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
