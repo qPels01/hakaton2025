542383e45817c867e81f6481725b2d78da7a796c
qPels01 <petrlensin011@gmail.com>
2025-05-30 21:07:53 +0300

    поправил лого хедера

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,7 +2,7 @@
   <header>
     <div class="logo">
       <h1>Байтики</h1>
-      <img id="icon-logo" src="../assets/header/Blogo.svg" />
+      <img src="../assets/header/Blogo.svg" />
     </div>
   </header>
 </template>
@@ -16,18 +16,20 @@ export default {
 <style>
 header {
   display: flex;
+  flex-direction: column;
   border-bottom: 1px solid #969595;
   height: 5rem;
   width: 100%;
-  padding: 30px 90px;
+  padding: 0 90px;
 }
-#icon-logo {
+.logo img {
   height: 2rem;
   width: 2rem;
+  display: block;
 }
 .logo {
   display: flex;
-  justify-content: center;
+  align-items: baseline;
   color: white;
   gap: 0.8rem;
 }
