c98d3317cf1e9c141e46ce2ebe8480956dd44b27
NikolyaVOLCKOV <96441256+NikolyaVOLCKOV@users.noreply.github.com>
2025-05-31 13:24:15 +0300

    Create show_tables.py

---
+++ backend/main-api/show_tables.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+# Параметры подключения
+conn = psycopg2.connect(
+    host="5.104.75.180",
+    port=5432,
+    dbname="neurodb",
+    user="neurouser",
+    password="neurosecret"
+)
+
+# Путь к SQL-файлу
+sql_file_path = "show.sql"
+
+with open(sql_file_path, "r", encoding="utf-8") as f:
+    sql = f.read()
+
+with conn:
+    with conn.cursor() as curs:
+        curs.execute(sql)
+        rows = curs.fetchall()
+        print("Таблицы в public-схеме PostgreSQL:")
+        for row in rows:
+            print(f"- {row[0]}")
+
+conn.close()
