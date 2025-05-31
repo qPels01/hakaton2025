b89e8b24dba6515be3d5616236697d6bc65164a0
qPels01 <petrlensin011@gmail.com>
2025-06-01 02:17:14 +0300

    Update TeamLog.vue

--- frontend/src/views/TeamLog.vue
+++ frontend/src/views/TeamLog.vue
@@ -1,57 +1,105 @@
 <template>
-  <div class="log-component">
-    <h1>Список разработчиков</h1>
-  </div>
-  <div class="teamer-container">
-    <div class="teamer">
-      <h1>Иванов Георгий Георгинович</h1>
-      <p>backend</p>
-      <p>python, SQLite</p>
-      <button class="teamer-btn">подробнее</button>
+  <div class="main-container">
+    <div class="head-component">
+      <h1>Список разработчиков</h1>
     </div>
+    <ul class="card-list">
+      <li v-for="item in teams" :key="item.id" class="card">
+        <span>Имя: {{ item.name }}</span>
+        <span>{{ item.role }}</span>
+        <span>Стек: {{ item.stack }}</span>
+        <button class="teamer-btn">подробнее</button>
+      </li>
+    </ul>
+    <button>Добавить разработчика</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "teamLOg",
+  data() {
+    return {
+      teams: [
+        {
+          stack: "React, Node.js, MongoDB Vue.js",
+          name: "Анна Иванова",
+          email: "anna.ivanova@example.com",
+          role: "frontend",
+        },
+        {
+          stack: "React, Node.js, MongoDB Vue.js",
+          name: "Сергей Петров",
+          email: "sergey.petrov@example.com",
+          role: "backend",
+        },
+        {
+          stack: "React, Node.js, MongoDB Vue.js",
+          name: "Мария Кузнецова",
+          email: "maria.kuznetsova@example.com",
+          role: "backend",
+        },
+        {
+          stack: "React, Node.js, MongoDB Vue.js",
+          name: "Дмитрий Сидоров",
+          email: "dmitry.sidorov@example.com",
+          role: "backend",
+        },
+        {
+          stack: "React, Node.js, MongoDB Vue.js",
+          name: "Елена Павлова",
+          email: "elena.pavlova@example.com",
+          role: "backend",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
-.log-component {
-  display: flex;
-  justify-content: center;
-  width: 100%;
+.teamer-btn {
+  max-width: 10rem;
+  padding: 0.5rem;
+  margin: 0;
 }
-.log-component h1 {
-  padding: 2rem;
+li {
+  list-style: none;
+}
+.main-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 3rem auto;
+  gap: 3rem;
+}
+.head-component {
+  width: 50%;
+  text-align: center;
+  margin: 2rem auto;
   border-bottom: 0.1rem solid #969595;
 }
-.teamer-container {
+.card-list {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  max-width: 70rem;
   width: 100%;
   gap: 3rem;
-  margin-top: 3rem;
+  margin: 0;
+  padding: 0;
 }
-.teamer {
+.card {
   display: flex;
-  gap: 10rem;
-  padding: 1rem;
+  justify-content: space-between;
   border-bottom: 0.1rem solid #969595;
+  align-items: center;
+  padding: 2rem;
+  gap: 5rem;
 }
-.teamer-btn {
-  width: auto;
-  max-width: none;
-  min-width: 9rem;
-  padding: 0.8em 2.5em;
-  font-size: 1rem;
-  border-radius: 2em;
-  padding: 0.5em 2em;
-  font-size: 1rem;
-  height: 2.5em;
-  line-height: 1.2;
-  align-self: center;
+span {
+  color: white;
+  font-size: 1.25rem;
 }
 </style>
